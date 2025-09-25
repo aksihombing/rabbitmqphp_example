b686bee062f0fd7e71b16dad1c5b9ec46581ca44
Rea <aksihombing05@gmail.com>
2025-09-25 15:43:52 -0400

    Added mysqlconnect.php file

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
+require_once('path.inc'); # I think we dont need this !!!! 
+require_once('get_host_info.inc'); # or this either !!!!!
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
